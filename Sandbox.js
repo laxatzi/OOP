f7b08af11ef329c18236639ab065efab57adce7a
laxatzi <laxatzi@yahoo.com>
2020-05-11 14:10:34 +0300

    update Sandbox.js and exercises.js files

--- Sandbox.js
+++ Sandbox.js
@@ -1,1 +1,10 @@
+//Ex #2
+  //Create a new instance of the House constructor, calling it myHouse and passing a number of bedrooms. Then, verify that it is an instance of House.
 
+  {
+   function House(numBedrooms) {
+      this.numBedrooms = numBedrooms;
+    }
+    
+      
+   }// end block
--- exercises.js
+++ exercises.js
@@ -1,1 +1,19 @@
 
+ //EX #1
+  //Create a Dog constructor. Set it up to take the parameters name and color, and have the property numLegs fixed at 4. Then create a new Dog saved in a variable terrier. Pass it two strings as arguments for the name and color properties
+
+//Ex #2
+  //Create a new instance of the House constructor, calling it myHouse and passing a number of bedrooms. Then, verify that it is an instance of House.
+
+  {
+   function House(numBedrooms) {
+      this.numBedrooms = numBedrooms;
+    }
+      
+   }// end block
+
+
+
+
+
+
