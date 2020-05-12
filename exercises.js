a047a9cf2f64715c422a9b4af241dc645b3b049b
laxatzi <laxatzi@yahoo.com>
2020-05-12 12:01:44 +0300

    modulize mixins

--- exercises.js
+++ exercises.js
@@ -158,3 +158,22 @@
        
        }       
    } // end block
+
+   //EX#13
+     // Create a module named funModule to wrap the two mixins isCuteMixin and singMixin. funModule should return an object.
+
+     {
+
+      let isCuteMixin = function(obj) {
+        obj.isCute = function() {
+          return true;
+        };
+      };
+      let singMixin = function(obj) {
+        obj.sing = function() {
+          console.log("Singing to an awesome tune");
+        };
+      };
+      
+     }//end block
+
